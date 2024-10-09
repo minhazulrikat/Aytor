e7d08277f808c95c78e4ebc164ec3faf1dd89806
Minhazul Rikat <minhazulrikat@example.com>
2024-10-09 20:12:54 +0600

    second upload

--- src/assets/components/Hero/Hero.jsx
+++ src/assets/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Hero1 from "../../images/hero1.png";
+import Hero2 from "../../images/hero2.png";
+import Hero3 from "../../images/hero3.png";
 
 const Hero = () => {
   return (
@@ -8,14 +10,14 @@ const Hero = () => {
         <div className="row">
           <div className="col-md-5">
             <div className="row">
-              <img className="img-fluid mt-3 mt-md-0" src="/src/assets/images/hero1.png" alt="" />
+              <img className="img-fluid mt-3 mt-md-0" src={Hero1} alt="" />
             </div>
             <div className="row">
-              <img className="img-fluid mt-3 mt-md-0" src="/src/assets/images/hero2.png" alt="" />
+              <img className="img-fluid mt-3 mt-md-0" src={Hero2} alt="" />
             </div>
           </div>
           <div className="col-md-7">
-            <img className="img-fluid mt-3 mt-md-0" src="/src/assets/images/hero3.png" alt="" />
+            <img className="img-fluid mt-3 mt-md-0" src={Hero3} alt="" />
           </div>
         </div>
       </div>
--- src/assets/components/products/Product.jsx
+++ src/assets/components/products/Product.jsx
@@ -6,10 +6,19 @@ import "./Product.css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import SubProduct from "./SubProduct/SubProduct";
+import image1 from "../../images/product1.png";
+import image2 from "../../images/product2.png";
+import image3 from "../../images/product3.png";
+import image4 from "../../images/product4.png";
+import image5 from "../../images/product5.png";
+import image6 from "../../images/product6.png";
+import image7 from "../../images/product7.jpg";
+import image8 from "../../images/product8.jpg";
+
 
 const Product = () => {
   return (
-    <div className="container">
+    <div className="container mb-5">
       <h2 className="text-center my-4">New Arrivals Products</h2>
       <Swiper
         navigation={true}
@@ -37,54 +46,70 @@ const Product = () => {
       >
         <SwiperSlide>
           <SubProduct
-          Sale = {true}
-          SaleText = {"-45%"}
-            image={"/src/assets/images/product1.png"}
-            image2={"/src/assets/images/product3.png"}
+            Sale={true}
+            SaleText={"-45%"}
+            image={image1}
+            image2={image3}
             price={"$150.65"}
             text={"Lotion Shower gel"}
           />
         </SwiperSlide>
         <SwiperSlide>
           <SubProduct
-           Sale = {true}
-           SaleText = {"Sale"}
-            image={"/src/assets/images/product2.png"}
-            image2={"/src/assets/images/product4.png"}
+            Sale={true}
+            SaleText={"Sale"}
+            image={image2}
+            image2={image4}
             price={"$256.00"}
             text={"Argan Cream Body Butter"}
           />
         </SwiperSlide>
         <SwiperSlide>
           <SubProduct
-            image={"/src/assets/images/product3.png"}
-            image2={"/src/assets/images/product1.png"}
+            image={image3}
+            image2={image1}
             price={"$175.04"}
             text={"Compact Face Powder"}
           />
         </SwiperSlide>
         <SwiperSlide>
           <SubProduct
-           Sale = {true}
-           SaleText = {"Sale"}
-            image={"/src/assets/images/product4.png"}
-            image2={"/src/assets/images/product3.png"}
+            Sale={true}
+            SaleText={"Sale"}
+            image={image4}
+            image2={image3}
             price={"$75.65"}
             text={"Manicure Gel nails, makeup"}
           />
         </SwiperSlide>
         <SwiperSlide>
           <SubProduct
-            image={"/src/assets/images/product8.jpg"}
-            image2={"/src/assets/images/product1.png"}
+            image={image8}
+            image2={image1}
             price={"$150.65"}
             text={"Lotion Shower gel"}
           />
         </SwiperSlide>
         <SwiperSlide>
           <SubProduct
-            image={"/src/assets/images/product7.jpg"}
-            image2={"/src/assets/images/product2.png"}
+            image={image7}
+            image2={image2}
+            price={"$150.65"}
+            text={"Lotion Shower gel"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SubProduct
+            image={image5}
+            image2={image4}
+            price={"$150.65"}
+            text={"Lotion Shower gel"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SubProduct
+            image={image6}
+            image2={image3}
             price={"$150.65"}
             text={"Lotion Shower gel"}
           />
